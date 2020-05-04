452bc606a5a8b38b7c32f1fc6aef716dd267dafd
James Holman <jameswarringtonholman@gmail.com>
2020-05-04 15:46:03 +1000

    :alembic: about to start event bus

--- comments/index.js
+++ comments/index.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-const PORT = 4001;
 
 const commentsByPostId = {};
 
@@ -18,18 +15,17 @@ app.get("/posts/:id/comments", (req, res) => {
 
 app.post("/posts/:id/comments", (req, res) => {
   const commentId = randomBytes(4).toString("hex");
-  const postId = req.params.id;
-
   const { content } = req.body;
 
-  const comments = commentsByPostId[postId] || [];
+  const comments = commentsByPostId[req.params.id] || [];
+
   comments.push({ id: commentId, content });
 
-  commentsByPostId[postId] = comments;
+  commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments);
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+app.listen(4001, () => {
+  console.log("Listening on 4001");
 });
--- posts/index.js
+++ posts/index.js
@@ -1,24 +1,22 @@
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const express = require("express");
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = 4000;
-
 const posts = {};
 
-app.get("/posts", (_req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
 app.post("/posts", (req, res) => {
   const id = randomBytes(4).toString("hex");
-
   const { title } = req.body;
+
   posts[id] = {
     id,
     title,
@@ -27,6 +25,6 @@ app.post("/posts", (req, res) => {
   res.status(201).send(posts[id]);
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+app.listen(4000, () => {
+  console.log("Listening on 4000");
 });
