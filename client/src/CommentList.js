cfde5dfe2096c394b3c7008c368281aa23b16767
James Holman <jameswarringtonholman@gmail.com>
2020-05-04 15:01:51 +1000

    :sparkles: client stuff

---
+++ client/src/CommentList.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+
+    setComments(res.data);
+  }, [postId]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
   const [posts, setPosts] = useState({});
@@ -23,6 +25,8 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
