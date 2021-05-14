0972be429e0394a094b5c04157fa0b8fb123fc12
James Holman <james.holman@domain.com.au>
2021-05-14 15:50:37 +1000

    feat: event history in event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,8 +6,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
+
   console.log({event})
 
   axios.post("http://localhost:4000/events", event);
@@ -17,6 +22,10 @@ app.post("/events", (req, res) => {
 
   res.send({ status: "OK" });
 });
+
+app.get('/events', (req, res) => {
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log("listening on 4005");
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   switch (type) {
     case "PostCreated":
       {
@@ -42,11 +37,25 @@ app.post("/events", (req, res) => {
     default:
       break;
   }
+}
 
-  console.log(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type,data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async() => {
   console.log("Listening on 4002");
+
+  // Get all events that have occured over time
+  const res = await axios.get('http://localhost:4005/events');
+  for (let event of res.data) {
+    console.log('Processing event: ', event.type);
+    handleEvent(event.type, event.data);
+  };
 });
