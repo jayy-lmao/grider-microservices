0b1a43c5327e467be5583e7d76fcd1b9969c5a65
James Holman <james.holman@domain.com.au>
2021-05-17 16:35:13 +1000

    feat: posts and event bus communicating

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,10 +15,10 @@ app.post("/events", (req, res) => {
 
   console.log({event})
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  axios.post("http://posts-clusterip-srv:4000/events", event);
+  // axios.post("http://localhost:4001/events", event);
+  // axios.post("http://localhost:4002/events", event);
+  // axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "OK" });
 });
