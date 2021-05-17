e1ca73b710921b416e2668189370ae60c3821443
James Holman <james.holman@domain.com.au>
2021-05-17 16:43:14 +1000

    feat: deployments for all services

--- query/index.js
+++ query/index.js
@@ -53,7 +53,7 @@ app.listen(4002, async() => {
   console.log("Listening on 4002");
 
   // Get all events that have occured over time
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://events-clusterip-srv:4005/events');
   for (let event of res.data) {
     console.log('Processing event: ', event.type);
     handleEvent(event.type, event.data);
